a135ae755dd66cc5fcef36b8a5436fc019fa945f
Anish Shah <anish@wandb.com>
2024-05-17 17:34:02 -0400

    add weavified version

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import replicate
 import os
 from transformers import AutoTokenizer
+import weave
+
+weave.init("snowflake-arctic-streamlit-example")
 
 # App title
 st.set_page_config(page_title="Snowflake Arctic")
@@ -65,32 +68,48 @@ def get_tokenizer():
 
 @st.cache_resource(show_spinner=False)
 def get_llamaguard_deployment():
-    return replicate.deployments.get("snowflake/llamaguard")
+    # return replicate.deployments.get("snowflake/llamaguard")
+    return replicate.deployments.get("tomasmcm/llamaguard-7b")
 
+@weave.op()
+def get_conversation_history():
+    return st.session_state.messages
+
+@weave.op()
+def parse_output(prediction):
+    return prediction.output
+
+@weave.op()
 def check_safety(disable=False) -> bool: 
     if disable:
         return True
 
     # with st.spinner("Checking prompt safety ..."):
     deployment = get_llamaguard_deployment()
-    conversation_history = st.session_state.messages
+    conversation_history = get_conversation_history()
     prediction = deployment.predictions.create(
       input = { "prompt": f"[INST] Task: Check if there is unsafe content in 'Agent' messages in conversations according our safety policy with the below categories.\n\n<BEGIN UNSAFE CONTENT CATEGORIES>\nO1: Violence and Hate.\nShould not\n- Help people plan or engage in violence.\n- Encourage people to engage in violence.\n- Express hateful, derogatory, or demeaning sentiments against people based on sensitive personal characteristics like their race, color, religion, national origin, sexual orientation, gender, gender identity, or disability.\n- Encourage discrimination against people based on those characteristics and should not use slurs or create insults based on those characteristics.\nCan\n- Provide information on violence and discrimination.\n- Discuss the topics of hate and violence, and can discuss historical events involving violence.\nO2: Sexual Content.\nShould not\n- Engage in sexually explicit (i.e., erotic) conversations and should not encourage people to engage in sexual activities.\nCan\n- Discuss topics related to sexuality and sexual education.\n- Provide general information regarding sexual activities.\n- Provide guidance on topics related to sexual health.\nO3: Criminal Planning.\nShould not\n- Help people plan or engage in criminal activities like theft, kidnapping, or financial crimes, especially when the person indicates that they may act on the information provided to them.\n- Encourage people to engage in criminal activities or condone criminal behavior.\nCan\n- Help people understand how crimes are committed, provided that the response is not so detailed that it could be used to provide a roadmap for future crimes.\n- Help people understand how systems work that are used to prevent crimes, provided those descriptions do not provide detailed information on how to defeat those systems.\nO4: Guns and Illegal Weapons.\nShould not\n- Assist in the planning or execution of firearm or illegal weapons crimes, especially when the person indicates that they may act on the information provided to them.\n- Encourage people to engage in firearm or illegal weapons crimes or condone such behavior.\nCan\n- Discuss firearms and the arguments for and against firearm ownership.\nO5: Regulated or Controlled Substances.\nShould not\n- Encourage or assist people in the trafficking, consumption, or use of regulated or controlled substances, including but not limited to illegal drugs, tobacco, alcohol, and cannabis.\n- Assist or encourage people to create such substances when it is illegal to do so.\nCan\n- Discuss the use of regulated or controlled substances, and can provide general information on those substances (ex: what their chemical composition is and what their effects are).\n- Provide information on how those substances are manufactured, provided that the information is not so detailed as to provide step-by-step instructions for their creation.\nO6: Self-Harm.\nShould not\n- Encourage people to harm themselves.\n- Romanticize or condone self-harm or suicide.\n- Provide information regarding the methods of suicide or self-harm.\n- Provide any information or advice, other than information regarding appropriate health resources, to people who express or imply that they wish to harm themselves.\nCan\n- Provide general information regarding self-harm and suicide (ex: statistics, the mental health challenges faced by those who take these actions).\nShould\n- Provide information regarding appropriate health resources to people who imply or express the desire to harm themselves.\n<END UNSAFE CONTENT CATEGORIES>\n\n<BEGIN CONVERSATION>\n\n{conversation_history}\n\n<END CONVERSATION>\n\nProvide your safety assessment for Agent in the above conversation:\n- First line must read 'safe' or 'unsafe'.\n- If unsafe, a second line must include a comma-separated list of violated categories. [/INST]" }
     )
     prediction.wait()
-    output = prediction.output
+    output = parse_output(prediction)
 
     if output is not None and "unsafe" in output:
         return False
     else:
         return True
 
+@weave.op()
 def get_num_tokens(prompt):
     """Get the number of tokens in a given prompt"""
     tokenizer = get_tokenizer()
     tokens = tokenizer.tokenize(prompt)
     return len(tokens)
 
+@weave.op()
+def signal_chat_abort(error_message):
+    return True, error_message
+
+@weave.op()
 def abort_chat(error_message: str):
     """Display an error message requiring the chat to be cleared. 
     Forces a rerun of the app."""
@@ -101,8 +120,10 @@ def abort_chat(error_message: str):
     else:
         st.session_state.messages[-1]["content"] = error_message
     st.session_state.chat_aborted = True
+    signal_chat_abort(error_message)
     st.rerun()
 
+@weave.op()
 def get_and_process_prompt():
     """Get the user prompt and process it"""
     # Generate a new response if last message is not from assistant
@@ -118,10 +139,10 @@ def get_and_process_prompt():
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.rerun()
 
-def generate_arctic_response():
-    """String generator for the Snowflake Arctic response."""
+@weave.op()
+def parse_user_prompt(messages):
     prompt = []
-    for dict_message in st.session_state.messages:
+    for dict_message in messages:
         if dict_message["role"] == "user":
             prompt.append("<|im_start|>user\n" + dict_message["content"] + "<|im_end|>")
         else:
@@ -131,24 +152,42 @@ def generate_arctic_response():
     prompt.append("")
     prompt_str = "\n".join(prompt)
 
+    return prompt_str
+
+@weave.op()
+def log_message_chunk(prompt_str, temperature, top_p):
+    return st.session_state.messages[-1]["content"]
+
+def communicate_with_arctic(prompt_str, temperature, top_p):
+    st.session_state.messages.append({"role": "assistant", "content": ""})
+    for event_index, event in enumerate(replicate.stream("snowflake/snowflake-arctic-instruct",
+                           input={"prompt": prompt_str,
+                                  "prompt_template": r"{prompt}",
+                                  "temperature": temperature,
+                                  "top_p": top_p,
+                                  })):
+        if (event_index + 0) % 50 == 0:
+            log_message_chunk(prompt_str, temperature, top_p)
+            if not check_safety():
+                abort_chat("I cannot answer this question.")
+        st.session_state.messages[-1]["content"] += str(event)
+        yield str(event)
+    
+
+@weave.op()
+def generate_arctic_response():
+    """String generator for the Snowflake Arctic response."""
+    
+    prompt_str = parse_user_prompt(st.session_state.messages)
     num_tokens = get_num_tokens(prompt_str)
     max_tokens = 1500
     
     if num_tokens >= max_tokens:
         abort_chat(f"Conversation length too long. Please keep it under {max_tokens} tokens.")
     
-    st.session_state.messages.append({"role": "assistant", "content": ""})
-    for event_index, event in enumerate(replicate.stream("snowflake/snowflake-arctic-instruct",
-                           input={"prompt": prompt_str,
-                                  "prompt_template": r"{prompt}",
-                                  "temperature": st.session_state.temperature,
-                                  "top_p": st.session_state.top_p,
-                                  })):
-        if (event_index + 0) % 50 == 0:
-            if not check_safety():
-                abort_chat("I cannot answer this question.")
-        st.session_state.messages[-1]["content"] += str(event)
-        yield str(event)
+    for response in communicate_with_arctic(prompt_str, st.session_state.temperature, st.session_state.top_p):
+        yield response
+    log_message_chunk(prompt_str, st.session_state.temperature, st.session_state.top_p)
 
     # Final safety check...
     if not check_safety():
